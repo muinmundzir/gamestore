53a3f471e7121fdc7ce7ba3fbc34d18d70dde901
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2021-09-14 12:09:36 +0700

    refactor edit profile page

--- components/organisms/Sidebar/MenuItem.tsx
+++ components/organisms/Sidebar/MenuItem.tsx
@@ -6,10 +6,11 @@ interface MenuItemProps {
   icon: 'icon-menu-card' | 'icon-menu-logout' | 'icon-menu-messages' | 'icon-menu-overview'|'icon-menu-rewards'|'icon-menu-settings'|'icon-menu-transactions';
   title: string;
   active?: boolean;
+  href?: string;
 }
 
 export default function MenuItem(props: Partial<MenuItemProps>) {
-  const { title, icon, active } = props;
+  const { title, icon, active, href } = props;
   const classItem = cx({
     item: true,
     'mb-30': true,
@@ -22,7 +23,7 @@ export default function MenuItem(props: Partial<MenuItemProps>) {
         <Image src={`/icon/${icon}.svg`} width={25} height={25} />
       </div>
       <p className="item-title m-0">
-        <Link href="/">
+        <Link href={href}>
           <a className="text-lg text-decoration-none">{title}</a>
         </Link>
       </p>
