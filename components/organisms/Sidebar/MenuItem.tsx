9db3b2f473a685eccf7cb2873acf85367c5c48ff
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2021-09-14 11:10:05 +0700

    refactor member sidebar

---
+++ components/organisms/Sidebar/MenuItem.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import cx from 'classnames';
+import Link from 'next/link';
+
+interface MenuItemProps {
+  icon: 'icon-menu-card' | 'icon-menu-logout' | 'icon-menu-messages' | 'icon-menu-overview'|'icon-menu-rewards'|'icon-menu-settings'|'icon-menu-transactions';
+  title: string;
+  active?: boolean;
+}
+
+export default function MenuItem(props: Partial<MenuItemProps>) {
+  const { title, icon, active } = props;
+  const classItem = cx({
+    item: true,
+    'mb-30': true,
+    active,
+  });
+
+  return (
+    <div className={classItem}>
+      <div className="me-3">
+        <Image src={`/icon/${icon}.svg`} width={25} height={25} />
+      </div>
+      <p className="item-title m-0">
+        <Link href="/">
+          <a className="text-lg text-decoration-none">{title}</a>
+        </Link>
+      </p>
+    </div>
+  );
+}
