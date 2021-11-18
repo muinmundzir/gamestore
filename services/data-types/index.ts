ecff064f90a1cff93c73e4a33fc6596bd010c3d6
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2021-11-18 14:05:29 +0700

    integrating nominals and payments

--- services/data-types/index.ts
+++ services/data-types/index.ts
@@ -11,3 +11,24 @@ export interface GameItemTypes {
   thumbnail:string,
   category: CategoryTypes
 }
+
+export interface BanksTypes {
+  _id: string;
+  name: string;
+  bankName: string;
+  accountNumber: string;
+}
+
+export interface PaymentTypes {
+  _id: string;
+  type: string;
+  status: string;
+  banks: BanksTypes[]
+}
+
+export interface NominalTypes {
+  _id: string;
+  coinQuantity: number;
+  coinName: string;
+  price: number
+}
