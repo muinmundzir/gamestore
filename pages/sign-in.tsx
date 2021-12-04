338cd63a91974c03471301edf842e97e376ef185
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2021-12-04 18:04:06 +0700

    edit single quote

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import Link from "next/link";
-import SignInForm from "../components/organisms/SignInForm";
+import Image from 'next/image';
+import Link from 'next/link';
+import SignInForm from '../components/organisms/SignInForm';
 
 export default function SignIn() {
   return (
