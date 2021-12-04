cb138308b64465d550c2b4b3c338a10087fa960f
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2021-12-04 19:24:48 +0700

    add data types

--- services/data-types/index.ts
+++ services/data-types/index.ts
@@ -1,14 +1,14 @@
 export interface CategoryTypes {
-  _id: string,
-  name: string,
+  _id: string;
+  name: string;
   __v: number
 }
 
 export interface GameItemTypes {
   _id: string;
-  status: string,
-  name: string,
-  thumbnail:string,
+  status: string;
+  name: string;
+  thumbnail:string;
   category: CategoryTypes
 }
 
@@ -34,6 +34,19 @@ export interface NominalTypes {
 }
 
 export interface LoginTypes {
-  email: string,
-  password: string
+  email: string;
+  password: string;
+}
+
+export interface UserTypes {
+  id: string;
+  username: string;
+  email: string;
+  name: string;
+  avatar: string;
+}
+
+export interface JWTPayloadTypes {
+  player: UserTypes;
+  iat: string;
 }
